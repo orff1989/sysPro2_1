d1946674210980c0b354a52a31283d84d480ae02
orff1989 <43110158+orff1989@users.noreply.github.com>
2022-03-07 22:37:06 +0200

    Add files via upload

---
+++ mat.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include "mat.hpp"
+
+using namespace std;
+
+char* ariel::mat(int c, int r, char ch1, char ch2) {
+    return NULL;
+}
---
+++ mat.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
+namespace ariel{
+    char* mat(int c, int r, char ch1, char ch2);   
+}
